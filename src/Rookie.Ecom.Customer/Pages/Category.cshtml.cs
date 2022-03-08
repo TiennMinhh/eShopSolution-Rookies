8ab663f11b754052babbdb3633bc6aafe304f33b
Hoàng Tiến <developer.tot@gmail.com>
2022-03-08 14:12:07 +0700

    Get data from bussiness to customer

--- src/Rookie.Ecom.Customer/Pages/Category.cshtml.cs
+++ src/Rookie.Ecom.Customer/Pages/Category.cshtml.cs
@@ -1,10 +1,25 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Rookie.Ecom.Business.Interfaces;
+using Rookie.Ecom.Contracts.Dtos;
+using System.Collections.Generic;
 
 namespace Rookie.Ecom.Customer.Pages
 {
     public class CategoryModel : PageModel
     {
+        private readonly ILogger<CategoryModel> _logger;
+        private readonly ICategoryService _categoryService;
+
+        public CategoryModel(ILogger<CategoryModel> logger, ICategoryService categoryService)
+        {
+            _logger = logger;
+            _categoryService = categoryService;
+        }
+
+        public IEnumerable<CategoryDto> listcategory => _categoryService.GetAllAsync().Result;
+
         public void OnGet()
         {
         }
